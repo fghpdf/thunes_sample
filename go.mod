8659df2ffc41903f307989253ecaa497142610c4
fghpdf <quxiangxuanqxx@gmail.com>
2020-11-04 11:49:11 +0800

    feat: cors

--- go.mod
+++ go.mod
@@ -4,6 +4,7 @@ go 1.15
 
 require (
 	github.com/biter777/countries v1.3.4
+	github.com/gin-contrib/cors v1.3.1
 	github.com/gin-contrib/requestid v0.0.1
 	github.com/gin-gonic/gin v1.6.3
 	github.com/go-playground/validator/v10 v10.4.1 // indirect
--- internal/pkg/routers/Init.go
+++ internal/pkg/routers/Init.go
@@ -1,6 +1,7 @@
 package routers
 
 import (
+	"github.com/gin-contrib/cors"
 	"github.com/gin-contrib/requestid"
 	"github.com/gin-gonic/gin"
 )
@@ -10,6 +11,7 @@ func Init() *gin.Engine {
 
 	r.Use(requestid.New())
 	r.Use(Logging())
+	r.Use(cors.Default())
 	r.Use(gin.Recovery())
 
 	return r
